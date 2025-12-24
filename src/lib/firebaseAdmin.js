c856c87c592b65efdc1150e3deeda74528b5b814
gtoovyrey <reymartcenteno03@gmail.com>
2025-12-24 12:46:15 +0800

    Final

--- src/lib/firebaseAdmin.js
+++ src/lib/firebaseAdmin.js
@@ -1,9 +1,17 @@
 import * as admin from 'firebase-admin';
 
 if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
-  });
+  if (process.env.GOOGLE_APPLICATION_CREDENTIALS) {
+    admin.initializeApp({
+      credential: admin.credential.applicationDefault(),
+    });
+  } else if (process.env.FIREBASE_SERVICE_ACCOUNT_KEY) {
+    admin.initializeApp({
+      credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY)),
+    });
+  } else {
+    console.error('Firebase Admin SDK could not be initialized. GOOGLE_APPLICATION_CREDENTIALS or FIREBASE_SERVICE_ACCOUNT_KEY environment variable is not set.');
+  }
 }
 
 export default admin;
